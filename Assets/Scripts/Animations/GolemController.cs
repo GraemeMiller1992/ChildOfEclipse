6987585b68977adebda52f6699f39d0a51a916a6
Aerids <eridsdale17@gmail.com>
2026-04-11 18:36:02 -0400

    Golem Animations

--- Assets/Scripts/Animations/GolemController.cs
+++ Assets/Scripts/Animations/GolemController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GolemController : MonoBehaviour
+public class MummyController : MonoBehaviour
 {
     private Animator animator;
     private NavMeshAgent agent;
